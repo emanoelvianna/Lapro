3fe4c5b40ed5b8d80ea22f09a2c856b168cf5f22
emanoelvianna <emanoel.vianna@outlook.com>
2016-01-04 23:48:10 -0200

    parcil - código para realizar a modificação de label

--- src/js/editionCampOfTube.js
+++ src/js/editionCampOfTube.js
@@ -1,9 +1,13 @@
+// como eu passo objeto para javascript? é assim: function my(Tubo tubo){} ?!
+
+
+// Tarefa: 1 - clicar no item x e sua label deve ser "Hello World"
+
 function my(){
     var t = document.getElementsByClassName("item");
     
-    for(var i = 0; i <= t.length; i++) {
-        var elemento = document.getElementsByClassName("item")[i];
+    for(var i = 1; i <= t.length; i++) {
+        var elemento = document.getElementsByClassName("item" + i);
         elemento.textContent = "Hello World";
-        console.log("olá");
     }
 }
