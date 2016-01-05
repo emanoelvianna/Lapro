1fa04e66e8c5736b9c6e525172e8d80c3fc6d1dd
Emanoel Vianna <emanoel.vianna@outlook.com>
2016-01-05 10:37:48 -0200

    Parcil - Edição simples da label do item

--- src/js/editionCampOfTube.js
+++ src/js/editionCampOfTube.js
@@ -1,16 +1,19 @@
 // como eu passo objeto para javascript? é assim: function my(Tubo tubo){} ?!
 
 
-// Tarefa: 1 - clicar no item x e sua label deve ser "Hello World"
+// Tarefa: 1.2 - clicar no item x e abrir open dialog
 
 function my(item){
     var t = document.getElementsByClassName("item");
     
     for(var i = 1; i <= t.length; i++) {
-        var atual = "item"+i;
-        if(atual == item) {
+        var itemAtual = "item"+i;
+        if(itemAtual == item) {
             var elemento = document.getElementsByClassName("item"+i)[0];
             elemento.textContent = "Hello World";
         }
     }
 }
+
+// Dúvidas: Por que eu preciso adicionar [0] ao final quando dou um getElements.. ?!
+// Dúvidas: É errado separar os js para cada funcionalidade?!
