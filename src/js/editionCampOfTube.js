9cf1979777740d46e88128d5be8d9eb34b1ab499
Emanoel Vianna <emanoel.vianna@outlook.com>
2016-01-08 12:40:19 -0200

    Tarefa1 - refatoração do mecanismo de edição.

--- src/js/editionCampOfTube.js
+++ src/js/editionCampOfTube.js
@@ -16,9 +16,9 @@ function salvar() {
   var codigo = document.getElementById("inputCodigo");
 
   for(var i = 1; i <= listaDeItens.length; i++) {
-    var itemAtual = "item "+i;
-    var itemClicado = document.getElementsByClassName(this.classItemClicado.replace('"', ""))[0];
-    if(itemAtual == itemClicado.textContent) {
+    var itemAtual = "item"+i;
+    var itemClicado = this.classItemClicado;
+    if(itemAtual == itemClicado) {
       var elemento = document.getElementsByClassName("item"+i)[0];
       elemento.textContent = codigo.value;
     }
