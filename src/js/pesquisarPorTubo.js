aa8380bf66e1fd7bc6d6a7025907106f09863693
Emanoel Vianna <emanoel.vianna@outlook.com>
2016-01-08 10:17:09 -0200

    Tarefa2 - mecanismo de busca de tubos.

--- src/js/pesquisarPorTubo.js
+++ src/js/pesquisarPorTubo.js
@@ -1,12 +1,14 @@
 // Tarefa2: buscar por tubo ao informar código no input e clicar no botão buscar.
 
-function buscar(itemProcurado) {
+function buscar() {
+    var elementoBuscado = document.getElementById("inputBusca").value;
     var listaDeItens = document.getElementsByClassName("item");
-    
+
     for(var i = 1; i <= listaDeItens.length; i++) {
-        var itemAtual = "item"+i;
-        if(itemProcurado == itemAtual) {
-            console.log("verdade");
+        var itemAtual = document.getElementsByClassName("item"+i)[0];
+
+        if(itemAtual.textContent == elementoBuscado) {
+            alert("Elemento encontrado!");
         }
     }
 }
