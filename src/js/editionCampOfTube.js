306a9f6874b8430c6b0909172305380ba9e96188
Emanoel Vianna <emanoel.vianna@outlook.com>
2016-01-08 12:27:34 -0200

    Tarefa1 - refatoração parcial

--- src/js/editionCampOfTube.js
+++ src/js/editionCampOfTube.js
@@ -1,9 +1,26 @@
+var classItemClicado;
+
 function editarItem(item) {
-  cacheInput(item);
+  this.classItemClicado = item;
+  cacheInput();
 }
 
-
-function cacheInput(item) {
+function cacheInput() {
   var input = document.getElementById("inputCodigo");
-  input.value = item;
+  var item = document.getElementsByClassName(this.classItemClicado.replace('"', ""))[0];
+  input.value = item.textContent;
+}
+
+function salvar() {
+  var listaDeItens = document.getElementsByClassName("item");
+  var codigo = document.getElementById("inputCodigo");
+
+  for(var i = 1; i <= listaDeItens.length; i++) {
+    var itemAtual = "item "+i;
+    var itemClicado = document.getElementsByClassName(this.classItemClicado.replace('"', ""))[0];
+    if(itemAtual == itemClicado.textContent) {
+      var elemento = document.getElementsByClassName("item"+i)[0];
+      elemento.textContent = codigo.value;
+    }
+  }
 }
