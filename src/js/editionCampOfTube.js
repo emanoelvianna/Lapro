be0c38476e7080e9afccfcb63585bbe67cc5d2af
Emanoel Vianna <emanoel.vianna@outlook.com>
2016-01-08 11:10:57 -0200

    Tarefa1 - refatoração do código.

--- src/js/editionCampOfTube.js
+++ src/js/editionCampOfTube.js
@@ -1,38 +1,9 @@
-// como eu passo objeto para javascript? é assim: function my(Tubo tubo){} ?!
-
-
-/* Continuação da Tarefa1:
-
-Ao clicar no modal ele deve trazer a informacao que esta atualmente no item.
-
-*/ 
-
-var item;
-var codigo;
-
-function setItem(item) {
-    this.item = item;
+function editarItem(item) {
+  cacheInput(item);
 }
 
-function getItem() {
-    return this.item;   
-}
 
-function editarItem() {
-    var listaDeItens = document.getElementsByClassName("item");
-    
-    this.codigo = document.getElementById("inputCodigo");
-    
-    for(var i = 1; i <= listaDeItens.length; i++) {
-        var itemAtual = "item"+i;
-        if(itemAtual == this.item) {
-            var elemento = document.getElementsByClassName("item"+i)[0];
-            elemento.textContent = codigo.value;
-        }
-    }
-}
-
-function cacheInput() {
-    var codigo = document.getElementById("inputCodigo");
-    codigo.textContent = item;
+function cacheInput(item) {
+  var input = document.getElementById("inputCodigo");
+  input.value = item;
 }
