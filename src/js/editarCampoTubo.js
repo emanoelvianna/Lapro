fc581f3f4470d1337c392c1f12bf7e407b7b1a94
Emanoel Vianna <emanoel.vianna@outlook.com>
2016-01-14 12:02:36 -0200

    Tarefa3 - refatoração dos códigos js

--- src/js/editarCampoTubo.js
+++ src/js/editarCampoTubo.js
@@ -1,19 +1,30 @@
 var classItemClicado;
 
-var editar = function(item) {
+var editarItem = function(item) {
   this.classItemClicado = item;
 }
 
+function retornaItem() {
+  return document.getElementsByClassName(this.classItemClicado.replace('"', ""))[0];
+}
+
+function retornaInput() {
+  return document.getElementById("inputCodigo");
+}
+
+function retornaListaDeItens(argument) {
+  return document.getElementsByClassName(argument);
+}
+
 var cacheInput = function() {
-  var input = document.getElementById("inputCodigo");
-  var item = document.getElementsByClassName(this.classItemClicado.replace('"', ""))[0];
+  var item = retornaItem();
+  var input = retornaInput();
   input.value = item.textContent;
-  return input;
 }
 
 var salvarInformacao = function() {
-  var listaDeItens = document.getElementsByClassName("item");
-  var codigo = document.getElementById("inputCodigo");
+  var listaDeItens = retornaListaDeItens("item");
+  var codigo = retornaInput();
 
   for(var i = 1; i <= listaDeItens.length; i++) {
     var itemAtual = "item"+i;
@@ -21,6 +32,8 @@ var salvarInformacao = function() {
     if(itemAtual == itemClicado) {
       var elemento = document.getElementsByClassName("item"+i)[0];
       elemento.textContent = codigo.value;
+      return true;
     }
   }
+  return false;
 }
