78d07467d37f5b94ba91af6a44f54e334a56099f
emanoelvianna <emanoel.vianna@outlook.com>
2016-01-28 22:29:08 -0200

    Tarefa3 - Teste de unidades editarCampoTubo.js e algumas refatorações

--- src/js/editarCampoTubo.js
+++ src/js/editarCampoTubo.js
@@ -18,18 +18,17 @@ function cacheInput() {
   var item = retornaItemClicado();
   var input = retornaInput("inputCodigo");
   input.value = item;
+  return input;
 };
 
 var botao = document.getElementById('salvar');
-botao.addEventListener("click", function() {
+botao.addEventListene("click", function() {
   var listaDeItens = retornaListaDeItens("item");
   var codigo = retornaInput("inputCodigo");
 
   for(var i = 1; i <= listaDeItens.length; i++) {
     var itemAtual = "item"+i;
-    console.log(itemAtual);
     var itemClicado = tubo.getclazzClicada();
-    console.log(itemClicado);
     if(itemAtual == itemClicado) {
       var elemento = document.getElementsByClassName("item"+i)[0];
       elemento.textContent = codigo.value;
--- test/editarCampoTuboSpec.js
+++ test/editarCampoTuboSpec.js
@@ -3,14 +3,19 @@ describe("Editar campos de tubos", function() {
   it("campo input deve trazer o código atual do item clicado", function() {
     var item = "item1";
     editar(item);
-    expect(classItemClicado).toEqual("item1");
+    expect(retornaItemClicado()).toEqual("item1");
   });
 
   it("retorno de lista de itens não deve ser vazio", function() {
     var item = "item";
     var list = retornaListaDeItens(item);
-    console.log(list.length);
     expect(list).not.toBeLessThan(0);
   });
 
+  it("modal deve trazer no campo input o código atual do tubo", function() {
+    var item = "item1";
+    editar(item);
+    var retorno = retornaItemClicado();
+    expect(retorno).toEqual(item);
+  });
 });
