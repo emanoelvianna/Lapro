29bd089afb9c631f736147fa01bc01de904e1abc
emanoelvianna <emanoel.vianna@outlook.com>
2016-02-02 14:53:27 -0200

    Tarefa3 - Parcial, teste de unidade Tubo.js

--- test/tuboSpec.js
+++ test/tuboSpec.js
@@ -1,9 +1,24 @@
 describe("Tubo:", function() {
 
-  it("o retorno da classe clicada não deve ser null", function() {
+  it("ao clicar no item sua classe não deve ser null", function() {
     var elemento = document.createElement('div');
     tubo.setClazzClicado(elemento);
     expect(retornaClazzClicada()).toBeTruthy();
+  });
+
+  it("ao clicar no item sua classe deve ser setada e o retorno deve ser igual", function functionName() {
+    var elemento = document.createElement("div");
+    elemento.setAttribute("id", "item1");
+    elemento.setAttribute("textContent", "item1");
+    elemento.setAttribute("class", "teste");
+    tubo.setClazzClicado(elemento);
+    expect(elemento).toEqual(tubo.getClazzClicado());
+  });
+
+  it("deve ser possivel setar o código no atributo _codigo", function functionName() {
+    var codigo = "12345";
+    tubo.setCodigo(codigo);
+    expect(codigo).toEqual(tubo.getCodigo());
   });
 
 })();
