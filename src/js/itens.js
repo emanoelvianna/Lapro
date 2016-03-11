ff41745764e5eb1bcfe436b9cf20affa79b183a9
Emanoel Vianna <emanoel.vianna@outlook.com>
2016-03-11 17:21:37 -0300

    Tarefa8 - parcial, mecanismo para criar novas gavetas

--- src/js/itens.js
+++ src/js/itens.js
@@ -6,15 +6,29 @@ function retornaInput(argument) {
 
 var button = document.getElementById('criar');
 button.addEventListener("click", function functionName() {
-  var quantidade = retornaInput("inputAmount").value;
-  var contador = 0;
-  while (contador < quantidade) {
-    var ele = document.createElement("div");
-    ele.setAttribute("id", "item" + contador);
-    ele.setAttribute("class", "item");
-    ele.innerHTML = "tubo " + contador;
-    var currentDiv = document.getElementsByClassName('itens');
-    currentDiv.insertBefore(ele, currentDiv.firstChild);
-    contador++;
-  }
+
+  //TODO: usar uma lista para gerar os elementos e ao final adicionar!
+
+  var elemento = document.createElement("li");
+  elemento.setAttribute("class", "item");
+  var textElemento = document.createTextNode("tubo");
+  elemento.appendChild(textElemento);
+
+  var list = document.getElementById("itens");
+  list.insertBefore(elemento, list.childNodes[0]);
+
+
+  /**
+    var quantidade = retornaInput("inputAmount").value;
+    var contador = 0;
+    while (contador < quantidade) {
+      var ele = document.createElement("div");
+      ele.setAttribute("id", "item" + contador);
+      ele.setAttribute("class", "item");
+      ele.innerHTML = "tubo " + contador;
+      var currentDiv = document.getElementsByClassName('itens');
+      currentDiv.insertBefore(ele, currentDiv.firstChild);
+      contador++;
+    }
+  **/
 });
