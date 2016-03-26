e608d249612b843a2a3a470387470a3fccc3810a
emanoelvianna <emanoel.vianna@outlook.com>
2016-03-25 23:40:40 -0300

    Tarefa8 - adicionando quantidade passada pelo campo

--- src/js/itens.js
+++ src/js/itens.js
@@ -4,10 +4,10 @@ function retornaInput(argument) {
   return document.getElementById(argument);
 }
 
-var button = document.getElementById('criar');
-button.addEventListener("click", function functionName() {
-
+var criar = document.getElementById('criar');
+criar.addEventListener("click", function functionName() {
   //TODO: usar uma lista para gerar os elementos e ao final adicionar!
+  var quantidade = document.getElementById('inputAmount').value;
 
   var novaLista = document.createElement("ul");
   novaLista.setAttribute("class", "itens1 estilo-itens");
@@ -15,15 +15,26 @@ button.addEventListener("click", function functionName() {
   var listItens = document.getElementById("lista");
   listItens.insertBefore(novaLista, listItens.childNodes[0]);
 
-  var elemento = document.createElement("li");
-  elemento.setAttribute("class", "item");
-  elemento.setAttribute("href", "#myModal");
-  elemento.setAttribute("data-toggle", "modal");
-  elemento.setAttribute("onclick", "tubo.setClazzClicado(this)");
-  var textElemento = document.createTextNode("tubo");
-  elemento.appendChild(textElemento);
+  for (var i = 0; i < quantidade; i++) {
+    var elemento = document.createElement("li");
+    elemento.setAttribute("class", "item");
+    elemento.setAttribute("href", "#myModal");
+    elemento.setAttribute("data-toggle", "modal");
+    elemento.setAttribute("onclick", "tubo.setClazzClicado(this)");
+    var textElemento = document.createTextNode("tubo" + i);
+    elemento.appendChild(textElemento);
+    var list = document.getElementsByClassName('itens1');
+    list[0].insertBefore(elemento, list[0].childNodes[0]);
 
-  var list = document.getElementsByClassName('itens1');
-	list[0].insertBefore(elemento, list[0].childNodes[0]);
+  }
+
 
 });
+
+function functionName() {
+
+}
+
+function criarNovoElemento() {
+
+}
